db1f3d8c0e08045a7562f3603e03f173ba9f9b6d
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-04-08 11:24:37 +0100

    logo pequenas sem bg-white

--- components/ui/watermark.tsx
+++ components/ui/watermark.tsx
@@ -3,20 +3,17 @@ import Image from "next/image";
 interface WatermarkProps {
   width?: number;
   height?: number;
-  className?: string;
 }
 
-export const Watermark = ({ width = 100, height = 100, className = "" }: WatermarkProps) => {
+export const Watermark = ({ width = 100, height = 100 }: WatermarkProps) => {
   return (
-    <div className={`w-full h-full ${className}`}>
+    <div>
       <Image
         src="/watermark.png"
         alt="Watermark"
         width={width}
         height={height}
-        className="w-full h-full object-cover"
-        priority
-        quality={100}
+        className="border-2 rounded-xl"
       />
     </div>
   );
