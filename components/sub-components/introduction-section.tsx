4d34bfbdabb704b69d10b81ef62a9a329f01e9ae
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-04-09 16:24:26 +0100

    1

--- components/sub-components/introduction-section.tsx
+++ components/sub-components/introduction-section.tsx
@@ -50,51 +50,126 @@ export default function IntroductionSection() {
           <h3 className="text-xl font-bold mb-2">INTRODUÇÃO À SAÚDE</h3>
   
           <p>
-            O acesso ao inconsciente exige compreensão e perspicácia. O meu objetivo não é complicar ou revolucionar, mas
-            simplificar permitindo que todos resgatem os conhecimentos esquecidos. O corpo humano, devido às constantes
-            transformações e contradições que nem a medicina consegue explicar totalmente, tem sido estudado há milênios.
+          O acesso ao inconsciente exige compreensão e perspicácia. O meu objetivo não é complicar
+          ou revolucionar, mas simplificar permitindo que todos resgatem os conhecimentos esquecidos.
           </p>
   
           <p>
-            Enquanto isso, a mente humana expande o poder da tecnologia encontrando verdades ou fragmentos dela além do
-            nosso planeta. O que hoje parece ficção em breve se tornará a nossa realidade. Sem dúvida, teremos acesso á
-            genialidade dos grandes homens que, ocupados com esse desenvolvimento, não tiveram tempo para desenvolver
-            também sistemas que nos ajudassem a entender as nossas emoções que causam tanta desarmonia entre a humanidade.
+          O corpo humano, devido às constantes transformações e contradições que nem a medicina
+consegue explicar totalmente, tem sido estudado há milênios. Enquanto isso, a mente humana
+expande o poder da tecnologia encontrando verdades ou fragmentos dela além do nosso
+planeta. O que hoje parece ficção em breve se tornará a nossa realidade. Sem dúvida, teremos
+acesso á genialidade dos grandes homens que, ocupados com esse desenvolvimento, não tiveram tempo
+para desenvolver também sistemas que nos ajudassem a entender as nossas emoções que causam tanta
+desarmonia entre a humanidade. A doença sempre foi um mistério diante do qual os grandes sonhos
+perdem a força e o mundo parece mais escuro do que na realidade o é.
           </p>
   
           <p>
-            A doença sempre foi um mistério diante do qual os grandes sonhos perdem a força e o mundo parece mais escuro
-            do que na realidade o é. Então, onde está a felicidade e a abundancia? Pesquisadores procuram provar forças
-            que atuam no nosso organismo. Sabem que nos vasos sanguíneos, fluem propriedades semelhantes à eletricidade,
-            (energia magnética e vibração), a que a medicina chama de "Sistema Nervoso Elétrico" e admitem cada vez mais,
-            que somos regidos por essa energia.
+          Então, onde está a felicidade e a abundancia? Pesquisadores procuram provar forças que
+atuam no nosso organismo. Sabem que nos vasos sanguíneos, fluem propriedades
+semelhantes à eletricidade, (energia magnética e vibração), a que a medicina chama de
+“Sistema Nervoso Elétrico” e admitem cada vez mais, que somos regidos por essa energia.
+Devido ao excesso da sua intelectualidade o seu ceticismo gera polêmica e afasta-nos do
+nosso verdadeiro “eu”. Hoje, cientistas já comprovam a Força Criadora e outras energias nos
+laboratórios.
           </p>
   
           <p>
-            Diante disso, percebemos que a doença não existe no sentido absoluto. A saúde é o estado natural do ser
-            humano, enquanto a doença surge como uma manifestação de emoções negativas, mágoa, ódio, medo e vingança que
-            se acumulam na nossa mente. Para que o nosso corpo fique livre dessas psicossomatizações, é essencial praticar
-            uma auto-reflexão sincera e reajustar a nossa harmonia interior.
+          Limitado pela densidade vivida, o ser humano definiu três dimensões visíveis, comprimento,
+largura e altura, ignorando a dimensão mental. A PNL (Programação Neurolinguística) ensina
+que somos vítimas da nossa própria criação mental e que o cérebro pode ser reprogramado a
+levar o inconsciente a acredita nesse novo programa, pois desde crianças, somos influenciados
+pelo ambiente em que vivemos e refletimos crenças alheias. Eu pretendo mostrar como
+ultrapassar essas crenças limitantes e explorar dimensões escondidas (planos inconscientes)
+para alcançar o controlo emocional. O seu equilíbrio nas várias vertentes da sua vida, Saúde,
+finanças, família, amor, dependem do equilíbrio entre razão e emoção, mas resistimos à
+
+mudança devido a crenças enraizadas que não permitem que vivamos o nosso verdadeiro Eu.
+Independentemente da sua crença e cultura, saiba que tudo pode ser transformado com novas
+escolhas, pois a cura não vem da capacidade intelectual, mas da perceção do próprio sistema
+interior. Enquanto a cura for procurada apenas no corpo físico, as pessoas permanecerão com
+perguntas sem respostas, ou incompletas, pois a doença não existe fisicamente ainda que
+diagnosticada como existente.
           </p>
   
           <p>
-            Isso exige compreender a origem dos conflitos que tivemos com os outros e reconciliarmo-nos com nós mesmos,
-            mesmo que aqueles que "hostilizamos" já não estejam neste plano.
+          Sobre a verdade destruída no passado e sobre a dificuldade do ser humano compreender os assuntos da
+força mental, o autor, Nelson Liano Jr., escreve num dos seus livros “As Habitantes do Ar “, o seguinte:
+”As correntes subterrâneas da prática e especulações religiosas foram relegadas àquelas que se
+encontravam em posição ameaçadora para as ortodoxias cristãs, que se lançaram às guerras santas das
+cruzadas. Os dogmas católicos impuseram-se na parte ocidental da Europa à força de massacres contra
+heréticos, expulsões e massacres de judeus, queima de bruxas, perseguições de grupos inteiros que
+ameaçavam a unidade da Europa sob a égide papal. A ideia do diabo tornou-se presente nos espíritos
+das pessoas que o viam como agente provocador de todos os males que afligiam a cristandade. As
+acusações de rituais assassínio e de prática de magia negra, as condenações e queimas de livros
+rabínicos, como o Talmud, liga-se ao combate e caça às bruxas, à guerra santa de extermínio, aos
+cátaros, à queima de cristãos novos e à criação da Santa Inquisição. Judeus, mulheres, místicos,
+revolucionários, seitas heterodoxas, todos destinados à fogueira purificatória. Os judeus assim como as
+bruxas eram acusados de praticar magia negra sob o disfarce da medicina, atentando contra a saúde dos
+cristãos. A peste negra que assolou a Europa no século XIV foi atribuída à ação maligna de judeus e
+bruxas. Sobre esse assunto o cronista alemão Conrad Von Nemegenberg faz o seguinte relato: ’Foram
+achados numerosos poços envenenados e um número incalculável de judeus, foi massacrado na
+Renânia, na Francônia e em todas as cidades alemãs. ”Para atingir as suas pretensões hegemônicas, a
+cristandade não se limitava a queimar pessoas vivas. A claridade provocada pelas chamas seculares das
+fogueiras, ao invés de iluminar o planeta trouxe as trevas, destruindo todo o tipo de conhecimento que
+ultrapassasse o limite imposto pelos dogmas da religião dominante. Ideologicamente, os juizes
+inquisitoriais queriam extirpar da cultura humana, tradições e costumes que ameaçassem o domínio do
+Vaticano que significava o centro do poder político da época. Assim como os nazistas, fascistas,
+stalinistas e outros tipos de ditadores centralizadores de poder, os enviados papais percorriam a Europa
+destruindo toda espécie de literatura filosófica que desafiasse a ’Verdade Divina’.” (grifo da autora)
           </p>
   
           <p>
-            A consciência tranquila é a chave para transformar não apenas a nossa saúde, mas também os nossos
-            relacionamentos, carreira, recuperação da saúde, profissional, amoroso ou qualquer outra situação que nos fez
-            somatizar um desequilíbrio energético no nosso corpo então, a cabeça, o tronco os membros e cada órgão
-            interno, recebem um impulso nervoso do cérebro, que é comandado pelas emoções e, com confiança e
-            desprendimento, podemos libertar a nossa mente de traumas e ressentimentos. Compreenda-se e liberte-se!
+          Outra autora admirável, Helene Bernard, no seu livro “As Grandes Iniciadas” escreve: ”Na verdade, a fé
+dos cátaros era tão grande que podia ser comparada à dos primeiros mártires do cristianismo. A sua
+doutrina era verdadeiramente inspirada pelo espírito do ideal cristão primitivo, antes de ele ter sido
+desvirtuado por algumas falsas interpretações. ”Receberam a morte pelo fogo com a mesma coragem dos
+mártires do tempo do império romano. E, entretanto, as suas crenças foram de tal modo desfiguradas
+pelos inquisidores que eles foram acusados de admitir o suicídio como um ato desejável. ”Toda ação
+provoca uma reação e o poder dos papas depois dessas lutas sangrentas, foi grandemente enfraquecido.
+Pela contínua e cruel violência do fanatismo dos inquisidores das suas perseguições aqueles que se lhes
+opunham, as populações dos países ocidentais rejeitaram a autoridade espiritual da Igreja.” Então querido
+leitor, vamos resgatar os conhecimentos que se encontram fragmentados pelo mundo, aceite alguma
+ajuda ”alternativa” e deixe-se levar pela magia dessa realidade! Deixe o saber para que o mundo todo
+volte a conhecer os princípios da mente pura e finalmente possa desatar os ”nós” da saúde.
           </p>
   
           <p>
-            A energia do amor depende da energia que emanamos (repelimos ou atraímos). Se não nos libertarmos das emoções
-            negativas, a nossa vida fica presa num círculo vicioso, criando doenças, acidentes e insucessos. A solução é
-            simples, mas poderosa: Perdoe!
+          O Vaticano e a Ordem Rosacruz possuem trancados nos cofres os pergaminhos originais da época de
+Jesus Cristo. Desta forma, faz deles os grandes mestres dos ”símbolos reais da salvação”. Mas, sabe-se
+que o Vaticano não transmite totalmente a verdade, restringindo-se, apenas, àquilo que lhe convém. As
+religiões proliferam e apenas provocam a desunião da Divindade, devido às interpretações variadas da
+Bíblia e das sutras. Corrompem os seus próprios princípios para garantirem as sua crença e acabaram
+por afastar a poderosa frase: ”Ama o teu próximo como a ti mesmo”.
           </p>
+
+          <p>
+          Diante disso, percebemos que a doença não existe no sentido absoluto. A saúde é o estado
+natural do ser humano, enquanto a doença surge como uma manifestação de emoções
+negativas, mágoa, ódio, medo e vingança que se acumulam na nossa mente. Para que o nosso
+corpo fique livre dessas psicossomatizações, é essencial praticar uma auto-reflexão sincera e
+reajustar a nossa harmonia interior. Isso exige compreender a origem dos conflitos que tivemos
+com os outros e reconciliarmo-nos com nós mesmos, mesmo que aqueles que “hostilizamos” já
+não estejam neste plano.
+          <p/>
+          
+          <p>
+          A consciência tranquila é a chave para transformar não apenas a nossa saúde, mas também
+os nossos relacionamentos, carreira, recuperação da saúde, profissional, amoroso ou qualquer
+outra situação que nos fez somatizar um desequilíbrio energético no nosso corpo então, a
+
+cabeça, o tronco os membros e cada órgão interno, recebem um impulso nervoso do cérebro,
+que é comandado pelas emoções e, com confiança e desprendimento, podemos libertar a
+nossa mente de traumas e ressentimentos. Compreenda-se e liberte-se!
+          <p/>
+
+          <p>
+          A energia do amor depende da energia que emanamos (repelimos ou atraímos). Se não nos
+libertarmos das emoções negativas, a nossa vida fica presa num círculo vicioso, criando
+doenças, acidentes e insucessos. A solução é simples, mas poderosa: Perdoe!
+          <p/>
+
   
           <div className="bg-blue-50 p-6 rounded-lg my-6 shadow-md">
             <h3 className="text-xl font-bold mb-2 text-blue-700">PERDÃO O QUE É?</h3>
