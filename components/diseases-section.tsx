c180ab96288f818ff5f4d2a1d0e78bcd4a446bd4
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-05-08 01:52:06 +0100

    ?

--- components/diseases-section.tsx
+++ components/diseases-section.tsx
@@ -4,19 +4,17 @@ import IntroductionSection from "./sub-components/introduction-section"
 import DiseasesList from "./sub-components/diseases-list"
 import { Watermark } from "./ui/watermark"
 
-
 export default function DiseasesSection() {
   return (
     <section id="doencas" className="relative py-24 bg-gradient-to-b from-white to-gray-100">
-  <div className="absolute inset-0 w-full h-full pointer-events-none flex items-center justify-center">
-  <div className="max-w-4xl max-h-[30vh]">
-  <Watermark />
-    </div>
-  </div>
+      <div className="fixed inset-0 w-full h-full pointer-events-none flex items-center justify-center">
+        <div className="w-full max-w-4xl h-full max-h-[30vh]">
+          <Watermark />
+        </div>
+      </div>
 
-      <div className="container mx-auto px-4">
+      <div className="container relative z-10 mx-auto px-4">
         <h2 className="mb-6 text-center text-3xl font-bold">Doenças Porquê?</h2>
-
         <IntroductionSection />
         <DiseasesList />
       </div>
