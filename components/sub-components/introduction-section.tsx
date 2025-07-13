820f5a6278fc0d60ff3daf25b5683808f575ec5c
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2025-07-13 16:28:02 +0100

    mudei texto

--- components/sub-components/introduction-section.tsx
+++ components/sub-components/introduction-section.tsx
@@ -48,26 +48,21 @@ export default function IntroductionSection() {
             </ul>
             <p className="text-center mt-4 font-bold text-green-700">CONSUMIR COM FREQUENCIA!</p>
           </div>
-          <p>ou ouça o audio</p>
+          <p>Meditação:  Cure as células doentes</p>
           <AudioPlayer src="/audio/audio.mp3" />
   
           <h3 className="text-xl font-bold mb-2">INTRODUÇÃO À SAÚDE</h3>
-  
-          <p>
-          O acesso ao inconsciente exige compreensão e perspicácia. O meu objetivo não é complicar
-          ou revolucionar, mas simplificar permitindo que todos resgatem os conhecimentos esquecidos.
-          </p>
-  
-  <p>
-    O corpo humano, devido às constantes transformações e contradições que nem a medicina consegue explicar totalmente, tem sido estudado há milênios. 
-    <br /> Enquanto isso, a mente humana expande o poder da tecnologia encontrando verdades ou fragmentos dela além do nosso planeta. O que hoje parece ficção, amanhã vai se tornar a nossa realidade.
-    <br /> A doença sempre foi um mistério diante do qual os grandes sonhos perdem a força e o mundo parece mais escuro do que na realidade é.
-    <br /> Pesquisadores procuram provar forças que atuam no nosso organismo. Sabem que nos vasos sanguíneos, fluem propriedades semelhantes à eletricidade, (energia magnética e vibração), a que a medicina chama de “Sistema Nervoso Elétrico” e admitem, cada vez mais, que somos regidos por essa energia”. Hoje, cientistas já comprovam a Força Criadora e outras energias nos laboratórios. 
-    <br /> Limitado pela densidade vivida, o ser humano definiu três dimensões visíveis, comprimento, largura e altura, ignorando a dimensão mental.
-    <br /> A PNL (Programação Neurolinguística) ensina que somos vítimas da nossa própria criação mental e que o cérebro pode ser reprogramado a levar o inconsciente a acredita nesse novo programa, pois desde crianças, somos influenciados pelo ambiente em que vivemos e refletimos crenças alheias.
-    <br /> Eu pretendo mostrar como ultrapassar essas crenças limitantes e explorar dimensões escondidas (planos inconscientes) para alcançar o controlo emocional.  Independentemente da sua crença e cultura, saiba que tudo pode ser transformado com novas escolhas, pois a cura não vem da capacidade intelectual, mas da perceção do próprio sistema interior.
-    <br /> Para que o nosso corpo fique livre dessas psicossomatizações, é essencial praticar uma auto-reflexão sincera e reajustar a nossa harmonia interior. Isso exige compreender a origem dos conflitos que tivemos com os outros e reconciliarmo-nos com nós mesmos
-  </p>
+
+          <p>O acesso ao inconsciente exige compreensão e perspicácia. O meu objetivo não é complicar ou revolucionar, mas simplificar permitindo que todos resgatem os conhecimentos esquecidos.
+          <br/> O corpo humano, devido às constantes transformações e contradições que nem a medicina consegue explicar totalmente, tem sido estudado há milênios. 
+          <br/> Enquanto isso, a mente humana expande o poder da tecnologia encontrando verdades ou fragmentos dela além do nosso planeta. O que hoje parece ficção, amanhã vai se tornar a nossa realidade.
+          <br/> A doença sempre foi um mistério diante do qual os grandes sonhos perdem a força e o mundo parece mais escuro do que na realidade é.
+          <br/> Pesquisadores procuram provar forças que atuam no nosso organismo. Sabem que nos vasos sanguíneos, fluem propriedades semelhantes à eletricidade, (energia magnética e vibração), a que a medicina chama de “Sistema Nervoso Elétrico” e admitem, cada vez mais, que somos regidos por essa energia”. Hoje, cientistas já comprovam a Força Criadora e outras energias nos laboratórios. 
+          <br/> Limitado pela densidade vivida, o ser humano definiu três dimensões visíveis, comprimento, largura e altura, ignorando a dimensão mental.
+          <br/> A PNL (Programação Neurolinguística) ensina que somos vítimas da nossa própria criação mental e que o cérebro pode ser reprogramado a levar o inconsciente a acredita nesse novo programa, pois desde crianças, somos influenciados pelo ambiente em que vivemos e refletimos crenças alheias.
+          <br/> Eu pretendo mostrar como ultrapassar essas crenças limitantes e explorar dimensões escondidas (planos inconscientes) para alcançar o controlo emocional.  Independentemente da sua crença e cultura, saiba que tudo pode ser transformado com novas escolhas, pois a cura não vem da capacidade intelectual, mas da perceção do próprio sistema interior.
+          <br/> Para que o nosso corpo fique livre dessas psicossomatizações, é essencial praticar uma auto-reflexão sincera e reajustar a nossa harmonia interior. Isso exige compreender a origem dos conflitos que tivemos com os outros e reconciliarmo-nos com nós mesmos
+        </p>
   
           <div className="bg-blue-50 p-6 rounded-lg my-6 shadow-md">
             <h3 className="text-xl font-bold mb-2 text-blue-700">PERDÃO O QUE É?</h3>
